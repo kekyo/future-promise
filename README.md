60ec25fe230d7dfbc5ac08ebab6eda867eb1e161
Kouji Matsui <k@kekyo.net>
2018-04-03 00:32:12 +0900

    Add headers.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # future-promise implements at C++ from scratch
+
+## License
+
+Apache v2
--- future-promise/stdafx.h
+++ future-promise/stdafx.h
@@ -1,12 +1,25 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
+//////////////////////////////////////////////////////////////////////////
 //
+// future-promise implements at C++ from scratch
+// Copyright (c) 2018 Kouji Matsui (@kekyo2)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//////////////////////////////////////////////////////////////////////////
 
 #pragma once
 
 #if _DEBUG
-#define _CRT_SECURE_NO_WARNINGS 1
 #define _CRTDBG_MAP_ALLOC 1
 #include <crtdbg.h>
 #endif
