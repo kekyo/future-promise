07bc63549f32f7f30df703f59711b5615d6eb7ae
Kouji Matsui <k@kekyo.net>
2018-04-03 00:28:13 +0900

    Fixed leaked memory.

--- future-promise/future-promise.cpp
+++ future-promise/future-promise.cpp
@@ -284,18 +284,20 @@ private:
 
     void entry()
     {
-        while (!abort_)
+        while (true)
         {
             std::unique_lock<std::mutex> lock(lock_);
 
+        loop:
             if (queue_.empty())
             {
-                cv_.wait(lock);
-
-                if (queue_.empty())
+                if (abort_)
                 {
-                    continue;
+                    break;
                 }
+
+                cv_.wait(lock);
+                goto loop;
             }
 
             auto continuation = queue_.front();
@@ -357,12 +359,21 @@ public:
 //////////////////////////////////////////////////////////////////////////
 // Test code
 
-static void test(const std::shared_ptr<synch_context>& scPtr)
+static unsigned int get_current_thread_id()
+{
+#ifdef WIN32
+    return ::GetCurrentThreadId();
+#else
+    return 0;
+#endif
+}
+
+static void test_core(const std::shared_ptr<synch_context>& scPtr)
 {
     {
         promise<int> p(scPtr);
         auto f = p.get_future();
-        f.then([](auto value) { printf("value1=%d, %d\n", value, ::GetCurrentThreadId()); });
+        f.then([](auto value) { printf("value1=%d, %u\n", value, get_current_thread_id()); });
 
         p.set_value(123);
     }
@@ -372,14 +383,14 @@ static void test(const std::shared_ptr<synch_context>& scPtr)
         p.set_value(456);
 
         auto f = p.get_future();
-        f.then([](auto value) { printf("value2=%d, %d\n", value, ::GetCurrentThreadId()); });
+        f.then([](auto value) { printf("value2=%d, %u\n", value, get_current_thread_id()); });
     }
 
     {
         promise<int> p(scPtr);
         auto f1 = p.get_future();
         auto f2 = f1.map<int>([](auto value) { return value + 1; });
-        f2.then([](auto value) { printf("value3=%d, %d\n", value, ::GetCurrentThreadId()); });
+        f2.then([](auto value) { printf("value3=%d, %u\n", value, get_current_thread_id()); });
 
         p.set_value(123);
     }
@@ -388,21 +399,29 @@ static void test(const std::shared_ptr<synch_context>& scPtr)
         promise<int> p(scPtr);
         auto f1 = p.get_future();
         auto f2 = f1.bind<int>([scPtr](auto value) { return future<int>::result(value + 2, scPtr); });
-        f2.then([](auto value) { printf("value4=%d, %d\n", value, ::GetCurrentThreadId()); });
+        f2.then([](auto value) { printf("value4=%d, %u\n", value, get_current_thread_id()); });
 
         p.set_value(123);
     }
 }
 
-static void test()
+static void test_dummy()
 {
-    //auto scPtr = std::make_shared<dummy_synch_context>();
-    //auto scPtr = std::make_shared<queued_synch_context>();
+    auto scPtr = std::make_shared<dummy_synch_context>();
+    test_core(scPtr);
+}
+
+static void test_queued()
+{
+    auto scPtr = std::make_shared<queued_synch_context>();
+    test_core(scPtr);
+    scPtr->consume();
+}
+
+static void test_threaded()
+{
     auto scPtr = std::make_shared<threaded_synch_context>(10);
-
-    test(scPtr);
-
-    //scPtr->consume();
+    test_core(scPtr);
     scPtr->join();
 }
 
@@ -412,7 +431,9 @@ int main()
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_CHECK_ALWAYS_DF);
 #endif
 
-    test();
+    test_dummy();
+    test_queued();
+    test_threaded();
 
 #ifdef _CRTDBG_ALLOC_MEM_DF
     _CrtDumpMemoryLeaks();
